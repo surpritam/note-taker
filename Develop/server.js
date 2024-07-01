598a8127d2bc44b14620e0c282a6eab0657626ca
Pritam Sur <sur.pritam9878@gmail.com>
2024-07-01 11:01:45 -0400

    Added API routest for get and post notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { fs } = require('fs');
+const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = 3001;
 
@@ -16,6 +17,40 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// API routes
+app.get('/api/notes', (req, res) => {
+    // console.log(path.join(__dirname, 'db', 'db.json'));
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Failed to read notes' });
+        } else {
+            res.json(JSON.parse(data));
+        }
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = { id: uuidv4(), ...req.body };
+    console.log(req.body);
+
+    fs.readFile(path.join(__dirname, 'db','db.json'), 'utf8', (err, data) => {
+        if (err) {
+            res.status(500).json({ error: 'Failed to read notes' });
+        } else {
+            const notes = JSON.parse(data);
+            notes.push(newNote);
+            fs.writeFile(path.join(__dirname, 'db','db.json'), JSON.stringify(notes, null, 2), (err) => {
+                if (err) {
+                    res.status(500).json({ error: 'Failed to save note' });
+                } else {
+                    res.json(newNote);
+                }
+            });
+        }
+    });
+});
+
+// Wild card route
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
