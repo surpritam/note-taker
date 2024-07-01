2a048d63ca1489a5d718d26290c3510b98fbff2d
Pritam Sur <sur.pritam9878@gmail.com>
2024-07-01 10:07:31 -0400

    Created new html routes

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { fs } = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// HTML routes
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT}`)
+);
